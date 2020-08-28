9443a7b3a85fdac0ef0b06ff531474524b5b2f37
Thiago Tavares <tvttavares@gmail.com>
2020-08-28 11:12:38 -0400

    Creation of Room entity

---
+++ meeting-room-manager-api/src/main/java/com/dio/meetingroommanagerapi/model/Room.java
@@ -0,0 +1,38 @@
+package com.dio.meetingroommanagerapi.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "room")
+@Data
+public class Room {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+
+	@Column(name = "name", nullable = false)
+	private String name;
+
+	@Column(name = "start_hour", nullable = false)
+	private String startHour;
+
+	@Column(name = "end_hour", nullable = false)
+	private String endHour;
+
+	public Room(long id, String name, String startHour, String endHour) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.startHour = startHour;
+		this.endHour = endHour;
+	}
+
+}
