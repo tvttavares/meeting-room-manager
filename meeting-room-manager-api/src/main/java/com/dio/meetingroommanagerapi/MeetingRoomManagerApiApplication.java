3b471dfa88c8a1bc41183b150f91e68efd4b191c
Thiago Tavares <tvttavares@gmail.com>
2020-08-28 10:30:03 -0400

    Initial commit

---
+++ meeting-room-manager-api/src/main/java/com/dio/meetingroommanagerapi/MeetingRoomManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.dio.meetingroommanagerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingRoomManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingRoomManagerApiApplication.class, args);
+	}
+
+}
