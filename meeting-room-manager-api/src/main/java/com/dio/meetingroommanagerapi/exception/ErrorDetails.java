5fced18f3cc548d734f215c56593e9ae9fc298ca
Thiago Tavares <tvttavares@gmail.com>
2020-08-28 11:13:29 -0400

    Inserting Exception Handler

---
+++ meeting-room-manager-api/src/main/java/com/dio/meetingroommanagerapi/exception/ErrorDetails.java
@@ -0,0 +1,23 @@
+package com.dio.meetingroommanagerapi.exception;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class ErrorDetails {
+
+	private Date timestamp;
+	private String message;
+	private String details;
+
+	public ErrorDetails(Date timestamp, String message, String details) {
+		super();
+		this.timestamp = timestamp;
+		this.message = message;
+		this.details = details;
+	}
+}
