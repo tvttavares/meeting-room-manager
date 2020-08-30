3afbb27d64aa925fa8706ad9feccf61d646a2b22
Thiago Tavares <tvttavares@gmail.com>
2020-08-30 15:09:03 -0400

    Solving CORS problem

--- meeting-room-manager-api/src/main/java/com/dio/meetingroommanagerapi/controller/RoomController.java
+++ meeting-room-manager-api/src/main/java/com/dio/meetingroommanagerapi/controller/RoomController.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,7 @@ import com.dio.meetingroommanagerapi.model.Room;
 import com.dio.meetingroommanagerapi.repository.RoomRepository;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/rooms")
 public class RoomController {
 
